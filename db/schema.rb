3a9a3f453fd42415b15af7f55dba4659b763f1fb
Matt Berteaux <mattyb678@gmail.com>
2014-12-15 10:43:13 -0700

    Able to add and delete todo list items to a todo list

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141214002647) do
+ActiveRecord::Schema.define(version: 20141214003316) do
+
+  create_table "todo_items", force: true do |t|
+    t.string   "content"
+    t.integer  "todo_list_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
 
   create_table "todo_lists", force: true do |t|
     t.string   "title"
